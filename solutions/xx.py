4264104f4ae4d4e7261caa8065485681b11a814d
Vansh Nagpal <2002vansh@gmail.com>
2024-12-10 18:08:03 -0500

    updated part 1 solution

--- solutions/xx.py
+++ solutions/xx.py
@@ -1,10 +1,33 @@
 # TODO replace with AOC day
 day: str = "00"
 
-# TODO replace with test input
+# TODO replace with test input and corresponding answers
 test: str = """
 """
+pt1_ans: int = 0
+pt2_ans: int = 0
 
-# TODO parse puzzle input
-with open(f'puzzles/{day}.txt', 'r') as f:
-    input = f.read()
+
+def parse_input(string: str):
+    ...
+
+
+def pt1():
+    ...
+
+
+def pt2():
+    ...
+
+
+def main(input: str):
+    # TODO parse input and test
+
+    assert pt1_ans == pt1(test)
+    assert pt2_ans == pt2(test)
+
+
+if __name__ == "__main__":
+    with open(f'puzzles/{day}.txt', 'r') as f:
+        input = f.read()
+        main(input)
