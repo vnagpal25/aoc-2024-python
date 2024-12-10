d28cec5f8521a5a6e942ef710a5d416e719702c6
Vansh Nagpal <2002vansh@gmail.com>
2024-12-10 17:36:46 -0500

    setting up repository

---
+++ solutions/xx.py
@@ -0,0 +1,10 @@
+# TODO replace with AOC day
+day: str = "00"
+
+# TODO replace with test input
+test: str = """
+"""
+
+# TODO parse puzzle input
+with open(f'puzzles/{day}.txt', 'r') as f:
+    input = f.read()
